c4d7b15586e829daa13b9f190544bc1823430709
Your Name <you@example.com>
2020-06-08 17:13:30 +0100

    changes monday afternoon

---
+++ app.js
@@ -0,0 +1,10 @@
+//tile 1 has 0% risk
+//tile 2 has 20% risk
+//tile 3 has 40% risk
+//tile 4 has 60% risk
+//tile 5 has 80% risk
+//tile 6 has 100% risk
+
+//grab the hand element
+document.querySelector("#hand")
+//make the hand element move
